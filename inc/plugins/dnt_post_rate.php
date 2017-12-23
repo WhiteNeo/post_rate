f5ee96e9217a978916b5d73512817125e882c2ba
Eldenroot <778890+Eldenroot@users.noreply.github.com>
2017-12-23 22:34:25 +0100

    Follow Facebook style for box and fix "jumping" images on mouse hover
    Now it looks much better. Pure CSS solution.
    
    Anyway we still need to make some adjustments for another part, I can take a look if you merge this commit.

--- inc/plugins/dnt_post_rate.php
+++ inc/plugins/dnt_post_rate.php
@@ -186,10 +186,10 @@ function dnt_post_rate_activate()
 	}
 
 	// Creating stylesheet...
-	$stylesheet_css = '.post_rate_list{position: absolute;z-index: 9999;border: 2px solid #0F5579;background:#fff;margin-left: -120px;margin-top: -86px;border-radius: 10px;}
+	$stylesheet_css = '.post_rate_list{position: absolute;z-index: 9999;background:#fff;margin-left: -120px;margin-top: -86px;border-radius: 40px;heigh: 52px;transition: opacity .15s; box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.08), 0 2px 2px rgba(0, 0, 0, 0.15);}
 .post_rate_button{color: #fff;text-shadow: 1px 1px 1px #000;height: 26px;line-height: 26px;padding: 0 10px;text-decoration: none;margin-left: 4px;display: inline-block;cursor:pointer;background: #202020;border-radius: 4px;font-size: 13px;background: #0F5579 !important}
-.post_rate_btn img{cursor:pointer;}
-.post_rate_btn img:hover{width:60px;height:60px;margin-top:-96px;transition:all ease 0.5s;}
+.post_rate_btn img{cursor:pointer; transform: scale(1.00); transition: all 0.25s ease-in;}
+.post_rate_btn img:hover{transform: scale(1.25);transition: all 0.25s ease-in;margin-top:-96px;}
 .ptr_list{display: none;position: absolute;background: #0b0a0a;color: #e4dada;padding: 6px;border-radius: 3px;font-size: 10px;}
 .dnt_prt_ulist > span{display:block}
 .pcl_list{text-shadow: 1px 1px 1px #000;padding: 10px;border-radius: 2px;-moz-border-radius: 2px;-webkit-border-radius: 2px;color: #fff;text-align:center;font-size: 13px;display: inline-block;}
