9fdaf2ec80fb3152ab9bb91586cfbb1a36797deb
Dark Neo <neogeoman@gmail.com>
2017-12-23 15:51:48 -0600

    Update dnt_post_rate.lang.php
    New lang vars for image tags...

--- inc/languages/english/dnt_post_rate.lang.php
+++ inc/languages/english/dnt_post_rate.lang.php
@@ -7,9 +7,15 @@
 
 $l['pcl_rate'] = "Rate Post";
 $l['pcl_rated'] = "Post rated successfully";
-$l['pcl_total'] = "This post has been rated {1} times.";
+$l['pcl_total'] = '<span style="display:block">This post has been rated {1} times.</span>';
 $l['pcl_total_best'] = "The best rated thread of this user with {1} times this month is:";
-$l['pcl_rates'] = " Rates of this month: ";
+$l['pcl_rates'] = '<span style="display:block">Rates of this month:</span>';
 $l['pcl_dont_rates'] = "This user has not post rates this month !!!";
 $l['dnt_prt_alert'] = '{1} has rated your message "{2}"';
 $l['myalerts_setting_dntprt'] = 'Receive an alert when someone rate one of my threads?';
+$l['pcl_like'] = "Like";
+$l['pcl_love'] = "Love";
+$l['pcl_wow'] = "Wow";
+$l['pcl_smile'] = "Smile";
+$l['pcl_cry'] = "Cry";
+$l['pcl_angry'] = "Angry";
